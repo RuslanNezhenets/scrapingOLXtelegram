c7dca0a2a2e0fb4a75005b7b581c6034232813f4
RuslanNezhenets <ruslaneshenets@gmail.com>
2023-06-07 13:58:30 +0300

    project done

---
+++ app.js
@@ -0,0 +1,83 @@
+const App = require('node-telegram-bot-api')
+const getFromOLX = require('./scraping')
+const fs = require("fs");
+require('dotenv').config()
+
+const TOKEN = process.env.TOKEN
+const CHAT = process.env.CHAT
+
+const bot = new App(TOKEN)
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+function escapeJson(jsonData) {
+    const symbols = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']
+    const ourRegex = new RegExp('([' + symbols.map(s => '\\' + s).join('|') + '])', 'g');
+
+    for (let key in jsonData) {
+        for (let ad of jsonData[key]) {
+            ad.name = ad.name.replace(ourRegex, '\\$1')
+            for (let key in ad.seller) {
+                ad.seller[key] = ad.seller[key].replace(ourRegex, '\\$1')
+            }
+        }
+    }
+}
+
+function beautify(jsonMessage) {
+    escapeJson(jsonMessage)
+
+    let messages = []
+    for (let key in jsonMessage) {
+        let stringOutput = `*${key}*:`
+        for (let ad of jsonMessage[key]) {
+            stringOutput += `\n[${ad.name}](${ad.src}): `
+            stringOutput += (`${ad.seller.author} на OLX з ${ad.seller.date} ` +
+            `з рейтингом \"${ad.seller.rating}\" та ${ad.seller.deliveries} успішних доставок\\.`)
+        }
+        messages.push(stringOutput)
+    }
+    return messages
+}
+
+async function sendMessageByToken(chatId, message) {
+    const array = beautify(message)
+    for (let item of array) {
+        await bot.sendMessage(chatId, item, {
+            'parse_mode': 'MarkdownV2',
+            'disable_web_page_preview': true
+        })
+        await sleep(400)
+    }
+}
+
+function checkHistory(history, data) {
+    let result = {}
+    for (let key in data) {
+        let temp = []
+        if(history.hasOwnProperty(key)) {
+            for (let ad of data[key]) {
+                if (!history[key].some(obj => obj.src === ad.src))
+                    temp.push(ad)
+            }
+            if (Object.keys(temp).length > 0)
+                result[key] = temp
+        } else result[key] = data[key]
+    }
+    return result
+}
+
+getFromOLX().then(async data => {
+    if (!fs.existsSync('history.json'))
+        fs.writeFileSync('history.json', '{}')
+
+    const history = JSON.parse(fs.readFileSync('history.json', 'utf8'))
+
+    const result = checkHistory(history, data)
+    const formattedData = JSON.stringify(data, null, 2)
+    fs.writeFileSync(`history.json`, formattedData)
+
+    await sendMessageByToken(CHAT, result)
+})
---
+++ scraping.js
@@ -0,0 +1,186 @@
+const chrome = require('selenium-webdriver/chrome')
+const {Builder, By, until} = require('selenium-webdriver')
+const fs = require('fs')
+require('dotenv').config()
+
+const EMAIL = process.env.EMAIL
+const PASSWORD = process.env.PASSWORD
+const URL = 'https://www.olx.ua/uk/'
+
+async function getFromOLX() {
+    try {
+        console.log("Процесс начат")
+        const data = await WebScrapingLocalTest()
+        console.log("Процесс завершён")
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function WebScrapingLocalTest() {
+    let driver
+    try {
+        const options = new chrome.Options()
+        options.addArguments('headless')
+        options.excludeSwitches(['enable-logging'])
+        driver = await new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(options)
+            .build()
+
+        await driver.get(URL)
+
+        await login(driver)
+
+        await driver.get('https://www.olx.ua/uk/favorites/search/')
+
+        const searchLinks = []
+        const searches = await driver.findElements(By.css(".box.rel.br4.observedsearch"))
+
+        for (let search of searches) {
+            const searchLink = await search.findElement(By.css(".searchLink"))
+            const value = await search.findElement(By.css(".block.fbold")).getText()
+            const href = await searchLink.getAttribute('href')
+            const text = await searchLink.getText()
+            const novelties = parseInt(text.match(/\d+/)[0])
+            if (novelties > 0)
+                searchLinks.push({href: href, value: value, novelties: novelties})
+        }
+
+        const ads = {}
+
+        for (let search of searchLinks) {
+            await driver.get(search.href)
+            console.log("Произвожу поиск по категории:", search.value)
+
+            await driver.sleep(3000)
+            const currentUrl = await driver.getCurrentUrl()
+            if (currentUrl !== search.href) {
+                await driver.get(search.href)
+            }
+
+            const cards = await driver.findElements(By.css('[data-cy="l-card"]'))
+            await driver.wait(async () => {
+                return cards.length > 0
+            }, 20000)
+
+            const ads_by_search = []
+            for (let card of cards) {
+                const novelty = await card.findElements(By.css('.css-1kyngsx.er34gjf0'))
+                if (novelty.length > 0) {
+
+                    const name = await card.findElement(By.css('.css-16v5mdi.er34gjf0')).getText()
+                    const src = await card.findElement(By.css('.css-rc5s2u')).getAttribute('href')
+                    ads_by_search.push({
+                        name: name ?? '',
+                        src: src ?? '',
+                        seller: {}
+                    })
+                }
+            }
+
+            for (let ad of ads_by_search) {
+                await driver.get(ad.src)
+
+                let author = await FindElementSafe(driver, By.css('.css-1lcz6o7.er34gjf0'), 5000)
+                if(author) author = await author.getText()
+                else author = null
+
+                const date_element = await driver.findElement(By.css('.css-16h6te1.er34gjf0'))
+                const date = await date_element.findElement(By.tagName('b')).getText()
+
+                let rating = await FindElementSafe(
+                    driver,
+                    By.css('[data-testid="sentiment-description"]'),
+                    5000
+                )
+                if(rating) {
+                    const rating_title = await FindElementSafe(
+                        driver, By.css('[data-testid="sentiment-title"]'), 1000
+                    )
+                    const total_ratings = await FindElementSafe(
+                        driver, By.css('[data-testid="total-ratings"]'), 1000
+                    )
+                    if(rating_title)
+                        rating = `${await rating_title.getText()} ${await total_ratings.getText()}`
+                    else
+                        rating = null
+                }
+
+                const deliveries_element = await FindElementSafe(
+                    driver, By.css('[data-testid="delivery-badge"]')
+                )
+                let deliveries = null
+                if(deliveries_element) {
+                    deliveries = await deliveries_element.findElement(By.tagName('b')).getText()
+                }
+
+                ad.seller = {
+                    author: author ?? '',
+                    date: date ?? '',
+                    rating: rating ?? '',
+                    deliveries: deliveries ?? ''
+                }
+            }
+
+            ads[search.value] = ads_by_search
+        }
+        return ads
+    } catch (error) {
+        throw new Error(error)
+    } finally {
+        await driver.quit()
+    }
+}
+
+async function login(driver) {
+    const cookiesExist = fs.existsSync('cookies.json')
+    if (!cookiesExist) {
+        const profile = await driver.findElement(By.xpath("//*[@id=\"my-account-link\"]/div"))
+        await profile.click()
+
+        await driver.sleep(1000)
+
+        const email = await driver.findElement(
+            By.xpath("//div[@id='__next']/div/div/div/div/main/div/div[3]/div/form/div/div/div/input")
+        )
+        await email.sendKeys(EMAIL)
+
+        const password = await driver.findElement(
+            By.xpath("//div[@id='__next']/div/div/div/div/main/div/div[3]/div/form/div[2]/div/div/div/input")
+        )
+        await password.sendKeys(PASSWORD)
+
+        const submit = await driver.findElement(
+            By.xpath("//*[@id=\"__next\"]/div/div/div/div/main/div/div[3]/div/form/button[2]")
+        )
+        await submit.click()
+
+        await driver.wait(until.urlIs("https://www.olx.ua/d/uk/myaccount/"), 20000)
+
+        if (fs.existsSync('cookies.json'))
+            fs.unlinkSync('cookies.json')
+
+        const currentCookies = await driver.manage().getCookies()
+        fs.writeFileSync('cookies.json', JSON.stringify(currentCookies))
+    } else {
+        const cookies = JSON.parse(fs.readFileSync('cookies.json', 'utf8'))
+        for (const cookie of cookies) {
+            await driver.manage().addCookie(cookie)
+        }
+    }
+}
+
+async function FindElementSafe(driver, locator, time = 0) {
+    try {
+        if(time)
+            return await driver.wait(until.elementLocated(locator), time)
+        else
+            return await driver.findElement(locator)
+    } catch (error) {
+        return null
+    }
+}
+
+module.exports = getFromOLX
